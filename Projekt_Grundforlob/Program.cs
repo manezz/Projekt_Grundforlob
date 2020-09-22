8439088849a7033301d768a057f613b3ab1e9912
OliverC-123 <71122249+OliverC-123@users.noreply.github.com>
2020-09-22 09:30:31 +0200

    Startet switch

--- Projekt_Grundforlob/Program.cs
+++ Projekt_Grundforlob/Program.cs
@@ -10,7 +10,13 @@ namespace Projekt_Grundforlob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hejsa");
+            Console.WriteLine("Davs, halløj og goddag. Velkommen til Christians Festival!");
+            Console.WriteLine("I har nu to valgmuligheder.\n1. Festivalsplads - Hvor i må slå lejr\n2. Vores webshop hvor du kan købe camping udstyr");
+            string valg1;//Skaber en string for at modtager bruger svar
+            valg1=Console.ReadLine();//modtager brugerens svar
+            switch (valg1)//udnytte brugerens svar til vores switch
+
+
         }
     }
 }
