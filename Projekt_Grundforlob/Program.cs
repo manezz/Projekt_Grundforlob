2aea64539221dea784bfbd4ba4fe0a60f0edd7da
Magnus <68356591+manezz@users.noreply.github.com>
2020-09-24 12:00:20 +0200

    sammensat min og olivers git

--- Projekt_Grundforlob/Program.cs
+++ Projekt_Grundforlob/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,148 +11,266 @@ namespace fix1
     {
         static void Main(string[] args)
         {           
-            double result;
+            double result = 0;
 
+            string valg1 = "";
+            string valg2 = "";
+            string valg3 = "";
+            string valg4 = "";
+            string valg5 = "";
+            string valg6 = "";
+
+            int talValg1 = 0;
+
+            int antalluft = 0;
+            int antalsove = 0;
+            int antaltelt = 0;
+
+            int i = 0;
+
+            string nummer = "tom";
+            int talNummer = 0;
+            int pladsNummer = 0;
+
+            string tvkant = "\x250C";
+            string thkant = "\x2510";
+            string bvkant = "\x2514";
+            string bhkant = "\x2518";
+
+            string vside = "\x2502";
+            string hside = "\x2502";
+            string top = "\x2500";
+            string bund = "\x2500";
+
+            string vaMid = "\x2500";
+            string loMid = "\x2502";
+
+            string kryds = "\x253C";
+
+            int brede = 80;
+            int lengde = 20;
+
+
+            while (i == 0)
             {
                 Console.WriteLine("Davs, halløj og goddag. Velkommen til Christians Festival!");
                 Console.WriteLine("I har nu to valgmuligheder.\n1. Festivalsplads - Hvor i må slå lejr\n2. Vores webshop hvor du kan købe camping udstyr");
-                string valg1;//Skaber en string for at modtager bruger svar
-                valg1 = Console.ReadLine();//modtager brugerens svar
-                switch (valg1)//udnytte brugerens svar til vores switch
-                {
-                    case "1"://case for festivalsplads
-                        {
-                            Console.WriteLine("Du har valgt 'Festivalsplads'\tindtast det første cifre i dit telefon nummer for at blive tildelt en teltplads");
-                            string valg2;
-                            valg2 = Console.ReadLine();
-                            switch (valg2)
-                            {
-                                case "0":// Ikke indgået i opgaven men vi er så flinke her i gruppe 1 at telefonnummere med 0 må godt være med til vores festival
-                                case "1":
-                                case "2"://flere cases giver samme resutalt før break
-                                case "3":
-                                    {
-                                        Console.WriteLine("Dit telefon nummer starter med 0, 1, 2 eller 3");
-                                        break;
-                                    }
-                                case "4":
-                                case "5":
-                                case "6":
-                                    {
-                                        Console.WriteLine("Dit telefon nummer starter med 4, 5 eller 6");
-                                        break;
-                                    }
-                                case "7":
-                                case "8":
-                                case "9":
-                                    {
-                                        Console.WriteLine("Dit telefon nummer starter med 7, 8 eller 9");
-                                        break;
-                                    }
 
+                valg1 = Console.ReadLine();  //modtager brugerens svar
+
+                if (valg1.Length == 1)
+                {
+                    try 
+                    {
+                        talValg1 = Convert.ToInt32(valg1);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Det er ikke et tal");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Det er mere end en cifre");
+                }
+
+                if (talValg1 == 1)
+                {
+                    i = 1;
+                }
+                else if (talValg1 == 2)
+                {
+                    i = 2;
+                }
+                else
+                {
+                    Console.WriteLine("Det er ikke et acceptabelt valg");
+                }
+            }
+
+            while (i == 1) 
+            {
+                Console.WriteLine("Skriv første cifre af dit tellefon nummer");
+
+                nummer = Console.ReadLine();
+
+                if (nummer.Length == 1)
+                {
+                    try 
+                    {
+                        talNummer = Convert.ToInt32(nummer);
+                        i = 0;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Det er ikke et tal");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Det er ikke kun en cifre");
+                }
+
+            if (talNummer >= 0 && talNummer <= 3)
+            {
+                pladsNummer = 1;
+            }
+            else if (talNummer >= 4 && talNummer <= 6)
+            {
+                pladsNummer = 2;
+            }
+            else if (talNummer >= 7 && talNummer <= 9)
+            {
+                pladsNummer = 3;
+            }
+
+            Console.WriteLine($"Dit plads nummer er {pladsNummer}");
+
+            for (int y = 1; y < brede; y++)
+            {
+                Console.SetCursorPosition(y + 30, 8);
+                Console.Write(top);
+
+                Console.SetCursorPosition(y + 30, 8 + lengde);
+                Console.Write(bund);
+
+                Console.SetCursorPosition(y + 30, 8 + lengde / 2);
+                Console.Write(vaMid);
+            }
+
+            for (int y = 1; y < lengde; y++)
+            {
+                Console.SetCursorPosition(30, y + 8);
+                Console.Write(vside);
+
+                Console.SetCursorPosition(30 + brede, y + 8);
+                Console.Write(hside);
+
+                Console.SetCursorPosition(30 + brede / 2, y + 8);
+                Console.Write(loMid);
+            }
+
+            Console.SetCursorPosition(30, 8);
+            Console.Write(tvkant);
+
+            Console.SetCursorPosition(30 + brede, 8);
+            Console.Write(thkant);
+
+            Console.SetCursorPosition(30, 8 + lengde);
+            Console.Write(bvkant);
+
+            Console.SetCursorPosition(30 + brede, 8 + lengde);
+            Console.Write(bhkant);
+
+            Console.SetCursorPosition(30 + brede / 2, 8 + lengde / 2);
+            Console.Write(kryds);
+
+            Console.SetCursorPosition(0, 12 + lengde);
+            }
+
+            while (i == 2)
+            {
+                Console.WriteLine("Du har indtastet '2' for vores webshop");
+                Console.WriteLine("---------------Menu---------------");
+                Console.WriteLine("Tryk 1 - for Telt\t tryk 2 - for luftmadras\t tryk 3 - for sovepose");
+                Console.WriteLine("-----Telt----------350,75kr");
+                Console.WriteLine("-----Luftmadras----255,00kr");
+                Console.WriteLine("-----Sovepose------150,50kr");
+
+                valg3 = Console.ReadLine();
+
+                switch (valg3)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Du har valgt Telt - til 350,75kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
+                            valg4 = Console.ReadLine();
+                            switch (valg4) 
+                            {
+                                case "Y":
+                                    {
+                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at telte");
+
+                                        antaltelt = Convert.ToInt32(Console.ReadLine());                                                  
+                                        result = antaltelt * 350.75;
+                                        Console.WriteLine("Du har valgt[0] telte til pris af 350,75kr - totalpris af [1]kr", antaltelt, result);
+
+                                        break;
+                                    }
+                                case "N":
+                                    {
+                                        Console.WriteLine("Dit valg er blevet annulleret");
+
+                                        break;
+                                    }
                             }
                             break;
                         }
-
                     case "2":
                         {
-                            Console.WriteLine("Du har indtastet '2' for vores webshop");
-                            Console.WriteLine("---------------Menu---------------");
-                            Console.WriteLine("Tryk 1 - for Telt\t tryk 2 - for luftmadras\t tryk 3 - for sovepose");
-                            Console.WriteLine("-----Telt----------350,75kr");
-                            Console.WriteLine("-----Luftmadras----255,00kr");
-                            Console.WriteLine("-----Sovepose------150,50kr");
-                            string valg3;
-                            valg3 = Console.ReadLine();
-                            switch (valg3)
-                            {
-                                case "1":
-                                    {
-                                        string valg4;
-                                        Console.WriteLine("Du har valgt Telt - til 350,75kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-                                        valg4 = Console.ReadLine();
-                                        switch (valg4) 
-                                        {
-                                            case "Y":
-                                                {
-                                                    Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at telte");
-                                                    int antaltelt;
-                                                    antaltelt = Convert.ToInt32(Console.ReadLine());                                                  
-                                                    result = antaltelt * 350.75;
-                                                    Console.WriteLine("Du har valgt[0] telte til pris af 350,75kr - totalpris af [1]kr", antaltelt, result);
-                                                    break;
-                                                }
-                                            case "N":
-                                                {
-                                                    Console.WriteLine("Dit valg er blevet annulleret");
-                                                    break;
-                                                }
-                                        }
-                                        break;
-                                    }
-                                case "2":
-                                    {
-                                        Console.WriteLine("Du har valgt luftmadras - til 255,00kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-                                        string valg5;
-                                        valg5 = Console.ReadLine();
-                                        switch (valg5)
-                                        {
-                                            case "Y":
-                                                {
-                                                    Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at luftmadraser");
-                                                    int antalluft = 0;
-                                                    antalluft = Convert.ToInt32(Console.ReadLine());
-                                                    result = antalluft * 255;
-                                                    Console.WriteLine("Du har valgt[0] luftmadraser til pris af 255,00kr - totalpris af [1]kr", antalluft, result);
-                                                    break;
-                                                }
-                                            case "N":
-                                                {
-                                                    Console.WriteLine("Dit valg er blevet annulleret");
-                                                    break;
-                                                }
-                                        }
-                                    
-                                    break;
-                                    }
-                                case "3":
-                                    {
-                                        Console.WriteLine("Du har valgt sovepose - til 150,50kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-                                        string valg6;
-                                        valg6 = Console.ReadLine();
-                                        switch (valg6)
-                                        {
-                                            case "Y":
-                                                {
-                                                    Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at soveposer");
-                                                    int antalsove;
-                                                    antalsove = Convert.ToInt32(Console.ReadLine());
-                                                    result = antalsove * 150.50;
-                                                    Console.WriteLine("Du har valgt[0] soveposer til pris af 255,00kr - totalpris af [1]kr", antalsove, result);
-                                                    break;
-                                                }
-                                            case "N":
-                                                {
-                                                    Console.WriteLine("Dit valg er blevet annulleret");
-                                                    break;
-                                                }
-                                        }
-                                        break;
-                                    }
+                            Console.WriteLine("Du har valgt luftmadras - til 255,00kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
 
-                                                                        
-                                    
-                                       
-                                    
-                                       
+                            valg5 = Console.ReadLine();
+
+                            switch (valg5)
+                            {
+                                case "Y":
+                                    {
+                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at luftmadraser");
+
+                                        antalluft = Convert.ToInt32(Console.ReadLine());
+                                        result = antalluft * 255;
+
+                                        Console.WriteLine("Du har valgt[0] luftmadraser til pris af 255,00kr - totalpris af [1]kr", antalluft, result);
+
+                                        break;
+                                    }
+                                case "N":
+                                    {
+                                        Console.WriteLine("Dit valg er blevet annulleret");
+
+                                        break;
+                                    }
                             }
+                                    
+                        break;
                         }
+                    case "3":
+                        {
+                            Console.WriteLine("Du har valgt sovepose - til 150,50kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
+
+                            valg6 = Console.ReadLine();
+
+                            switch (valg6)
+                            {
+                                case "Y":
+                                    {
+                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at soveposer");
+
+                                        antalsove = Convert.ToInt32(Console.ReadLine());
+                                        result = antalsove * 150.50;
+
+                                        Console.WriteLine("Du har valgt[0] soveposer til pris af 255,00kr - totalpris af [1]kr", antalsove, result);
+
+                                        break;
+                                    }
+                                case "N":
+                                    {
+                                        Console.WriteLine("Dit valg er blevet annulleret");
+
+                                        break;
+                                    }
+                            }
                             break;
-                        }
+                        }     
+                }
             }
+
+
             Console.WriteLine("Tryk for at lukke");
             Console.ReadKey();
         }
     }
 }
+
 
