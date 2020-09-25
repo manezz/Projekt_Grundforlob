aee9133ed08f2aacb95360a5bd5a638904d1f919
Magnus <68356591+manezz@users.noreply.github.com>
2020-09-25 09:54:27 +0200

    comit all

--- Projekt_Grundforlob/Program.cs
+++ Projekt_Grundforlob/Program.cs
@@ -111,165 +111,164 @@ namespace fix1
                     Console.WriteLine("Det er ikke kun en cifre");
                 }
 
-            if (talNummer >= 0 && talNummer <= 3)
-            {
-                pladsNummer = 1;
-            }
-            else if (talNummer >= 4 && talNummer <= 6)
-            {
-                pladsNummer = 2;
-            }
-            else if (talNummer >= 7 && talNummer <= 9)
-            {
-                pladsNummer = 3;
-            }
-
-            Console.WriteLine($"Dit plads nummer er {pladsNummer}");
-
-            for (int y = 1; y < brede; y++)
-            {
-                Console.SetCursorPosition(y + 30, 8);
-                Console.Write(top);
-
-                Console.SetCursorPosition(y + 30, 8 + lengde);
-                Console.Write(bund);
-
-                Console.SetCursorPosition(y + 30, 8 + lengde / 2);
-                Console.Write(vaMid);
-            }
-
-            for (int y = 1; y < lengde; y++)
-            {
-                Console.SetCursorPosition(30, y + 8);
-                Console.Write(vside);
-
-                Console.SetCursorPosition(30 + brede, y + 8);
-                Console.Write(hside);
-
-                Console.SetCursorPosition(30 + brede / 2, y + 8);
-                Console.Write(loMid);
-            }
-
-            Console.SetCursorPosition(30, 8);
-            Console.Write(tvkant);
-
-            Console.SetCursorPosition(30 + brede, 8);
-            Console.Write(thkant);
-
-            Console.SetCursorPosition(30, 8 + lengde);
-            Console.Write(bvkant);
-
-            Console.SetCursorPosition(30 + brede, 8 + lengde);
-            Console.Write(bhkant);
-
-            Console.SetCursorPosition(30 + brede / 2, 8 + lengde / 2);
-            Console.Write(kryds);
-
-            Console.SetCursorPosition(0, 12 + lengde);
-            }
-
-            while (i == 2)
-            {
-                Console.WriteLine("Du har indtastet '2' for vores webshop");
-                Console.WriteLine("---------------Menu---------------");
-                Console.WriteLine("Tryk 1 - for Telt\t tryk 2 - for luftmadras\t tryk 3 - for sovepose");
-                Console.WriteLine("-----Telt----------350,75kr");
-                Console.WriteLine("-----Luftmadras----255,00kr");
-                Console.WriteLine("-----Sovepose------150,50kr");
-
-                valg3 = Console.ReadLine();
-
-                switch (valg3)
+                if (talNummer >= 0 && talNummer <= 3)
                 {
-                    case "1":
-                        {
-                            Console.WriteLine("Du har valgt Telt - til 350,75kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-                            valg4 = Console.ReadLine();
-                            switch (valg4) 
-                            {
-                                case "Y":
-                                    {
-                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at telte");
-
-                                        antaltelt = Convert.ToInt32(Console.ReadLine());                                                  
-                                        result = antaltelt * 350.75;
-                                        Console.WriteLine("Du har valgt[0] telte til pris af 350,75kr - totalpris af [1]kr", antaltelt, result);
-
-                                        break;
-                                    }
-                                case "N":
-                                    {
-                                        Console.WriteLine("Dit valg er blevet annulleret");
-
-                                        break;
-                                    }
-                            }
-                            break;
-                        }
-                    case "2":
-                        {
-                            Console.WriteLine("Du har valgt luftmadras - til 255,00kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-
-                            valg5 = Console.ReadLine();
-
-                            switch (valg5)
-                            {
-                                case "Y":
-                                    {
-                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at luftmadraser");
-
-                                        antalluft = Convert.ToInt32(Console.ReadLine());
-                                        result = antalluft * 255;
-
-                                        Console.WriteLine("Du har valgt[0] luftmadraser til pris af 255,00kr - totalpris af [1]kr", antalluft, result);
-
-                                        break;
-                                    }
-                                case "N":
-                                    {
-                                        Console.WriteLine("Dit valg er blevet annulleret");
-
-                                        break;
-                                    }
-                            }
-                                    
-                        break;
-                        }
-                    case "3":
-                        {
-                            Console.WriteLine("Du har valgt sovepose - til 150,50kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
-
-                            valg6 = Console.ReadLine();
-
-                            switch (valg6)
-                            {
-                                case "Y":
-                                    {
-                                        Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at soveposer");
-
-                                        antalsove = Convert.ToInt32(Console.ReadLine());
-                                        result = antalsove * 150.50;
-
-                                        Console.WriteLine("Du har valgt[0] soveposer til pris af 255,00kr - totalpris af [1]kr", antalsove, result);
-
-                                        break;
-                                    }
-                                case "N":
-                                    {
-                                        Console.WriteLine("Dit valg er blevet annulleret");
-
-                                        break;
-                                    }
-                            }
-                            break;
-                        }     
+                    pladsNummer = 1;
                 }
-            }
+                else if (talNummer >= 4 && talNummer <= 6)
+                {
+                    pladsNummer = 2;
+                }
+                else if (talNummer >= 7 && talNummer <= 9)
+                {
+                    pladsNummer = 3;
+                }
+
+                Console.WriteLine($"Dit plads nummer er {pladsNummer}");
+
+                for (int y = 1; y < brede; y++)
+                {
+                    Console.SetCursorPosition(y + 30, 8);
+                    Console.Write(top);
+
+                    Console.SetCursorPosition(y + 30, 8 + lengde);
+                    Console.Write(bund);
+
+                    Console.SetCursorPosition(y + 30, 8 + lengde / 2);
+                    Console.Write(vaMid);
+                }
+
+                for (int y = 1; y < lengde; y++)
+                {
+                    Console.SetCursorPosition(30, y + 8);
+                    Console.Write(vside);
+
+                    Console.SetCursorPosition(30 + brede, y + 8);
+                    Console.Write(hside);
+
+                    Console.SetCursorPosition(30 + brede / 2, y + 8);
+                    Console.Write(loMid);
+                }
+
+                Console.SetCursorPosition(30, 8);
+                Console.Write(tvkant);
+
+                Console.SetCursorPosition(30 + brede, 8);
+                Console.Write(thkant);
+
+                Console.SetCursorPosition(30, 8 + lengde);
+                Console.Write(bvkant);
+
+                Console.SetCursorPosition(30 + brede, 8 + lengde);
+                Console.Write(bhkant);
+
+                Console.SetCursorPosition(30 + brede / 2, 8 + lengde / 2);
+                Console.Write(kryds);
+
+                Console.SetCursorPosition(0, 12 + lengde);
+                }
+
+                while (i == 2)
+                {
+                    Console.WriteLine("Du har indtastet '2' for vores webshop");
+                    Console.WriteLine("---------------Menu---------------");
+                    Console.WriteLine("Tryk 1 - for Telt\t tryk 2 - for luftmadras\t tryk 3 - for sovepose");
+                    Console.WriteLine("-----Telt----------350,75kr");
+                    Console.WriteLine("-----Luftmadras----255,00kr");
+                    Console.WriteLine("-----Sovepose------150,50kr");
+
+                    valg3 = Console.ReadLine();
+
+                    switch (valg3)
+                    {
+                        case "1":
+                            {
+                                Console.WriteLine("Du har valgt Telt - til 350,75kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
+                                valg4 = Console.ReadLine();
+                                switch (valg4) 
+                                {
+                                    case "Y":
+                                        {
+                                            Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at telte");
+
+                                            antaltelt = Convert.ToInt32(Console.ReadLine());                                                  
+                                            result = antaltelt * 350.75;
+                                            Console.WriteLine("Du har valgt[0] telte til pris af 350,75kr - totalpris af [1]kr", antaltelt, result);
+
+                                            break;
+                                        }
+                                    case "N":
+                                        {
+                                            Console.WriteLine("Dit valg er blevet annulleret");
+
+                                            break;
+                                        }
+                                }
+                                break;
+                            }
+                        case "2":
+                            {
+                                Console.WriteLine("Du har valgt luftmadras - til 255,00kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
+
+                                valg5 = Console.ReadLine();
+
+                                switch (valg5)
+                                {
+                                    case "Y":
+                                        {
+                                            Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at luftmadraser");
+
+                                            antalluft = Convert.ToInt32(Console.ReadLine());
+                                            result = antalluft * 255;
+
+                                            Console.WriteLine("Du har valgt[0] luftmadraser til pris af 255,00kr - totalpris af [1]kr", antalluft, result);
+
+                                            break;
+                                        }
+                                    case "N":
+                                        {
+                                            Console.WriteLine("Dit valg er blevet annulleret");
+
+                                            break;
+                                        }
+                                }
+                                    
+                            break;
+                            }
+                        case "3":
+                            {
+                                Console.WriteLine("Du har valgt sovepose - til 150,50kr. Tryk 'Y' hvis du vil købe et telt eller 'N' hvis du fortryder");
+
+                                valg6 = Console.ReadLine();
+
+                                switch (valg6)
+                                {
+                                    case "Y":
+                                        {
+                                            Console.WriteLine("Vi har godkendt dit køb. Vælg nu antal at soveposer");
+
+                                            antalsove = Convert.ToInt32(Console.ReadLine());
+                                            result = antalsove * 150.50;
+
+                                            Console.WriteLine("Du har valgt[0] soveposer til pris af 255,00kr - totalpris af [1]kr", antalsove, result);
+
+                                            break;
+                                        }
+                                    case "N":
+                                        {
+                                            Console.WriteLine("Dit valg er blevet annulleret");
+
+                                            break;
+                                        }
+                                }
+                                break;
+                            }     
+                    }
+                }
 
 
             Console.WriteLine("Tryk for at lukke");
             Console.ReadKey();
-        {
         }
     }
 }
